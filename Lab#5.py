e7b7b129f6f78c8bfae1b5f5a18b869a549a18a7
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2022-11-10 17:37:12 -0800

    Update and rename Lab#5 to Lab#5.py

---
+++ Lab#5.py
@@ -0,0 +1,16 @@
+1) Implement the 7-segment LEDs control based on the above example program
+
+https://youtube.com/shorts/0gavWrIjv-w
+
+2) Add one more 7-segment LED to the above design for the continuous display of 
+the decimal number from 1 to 25 and alphabet from A-Z display.
+
+
+
+
+Thank you! 
+
+Source Code: 
+
+
+             
